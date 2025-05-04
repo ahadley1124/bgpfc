bb33f212731ae88d0705909a5eec00b926c92dea
ahadley1124 <admin@austinh.dev>
2025-05-04 15:10:51 -0400

    KeepAlive size is so random between implimentations

--- src/impliments.rs
+++ src/impliments.rs
@@ -255,7 +255,9 @@ impl structs::keepaliveMessage {
     }
 
     pub fn from_bytes(bytes: &[u8]) -> Result<Self, &'static str> {
-        if bytes.len() <= 19 {
+        // the rfc says that the keepalive message is 19 bytes long, but multiple different implementations have different lengths
+        // so we will just check if the length is at least 19 bytes
+        if bytes.len() >= 19 {
             return Err("Invalid keepalive message length");
         }
         let header = structs::Header::from_bytes(&bytes[0..19])?;
