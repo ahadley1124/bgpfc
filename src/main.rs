92c49cb56cdb967894558bcae436b8f0d8d2196d
ahadley1124 <admin@austinh.dev>
2025-05-12 17:13:53 -0400

    Add debug vs release comments and flatten some of the code

--- src/main.rs
+++ src/main.rs
@@ -47,83 +47,80 @@ fn main() {
                         break;
                     } // Connection closed
                     Ok(n) => {
-                        let header = parse_header(buf);
-                        match header {
+                        let header = match parse_header(buf) {
                             Ok(header) => {
-                                match header.message_type {
-                                    1 => {
-                                        println!("Received open message");
-                                        let open_message = structs::openMessage::from_bytes(
-                                            &buf[19..n]
-                                        );
-                                        match open_message {
-                                            Ok(open_message) => {
-                                                println!(
-                                                    "Received open message: {:?}",
-                                                    open_message
-                                                );
-                                                // Send keepalive after successful open message parse
-                                                let keepalive = structs::keepaliveMessage::new(header);
-                                                let keepalive_bytes = keepalive.to_bytes();
-                                                if let Err(e) = stream.write_all(&keepalive_bytes) {
-                                                    eprintln!("Failed to send keepalive: {}", e);
-                                                } else {
-                                                    println!("Sent keepalive message");
-                                                }
-                                            }
-                                            Err(e) =>
-                                                eprintln!("Error parsing open message: {}", e),
-                                        }
-                                    }
-                                    2 => {
-                                        println!("Received update message");
-                                        let update_message = structs::updateMessage::from_bytes(
-                                            &buf[19..n]
-                                        );
-                                        match update_message {
-                                            Ok(update_message) =>
-                                                println!(
-                                                    "Received update message: {:?}",
-                                                    update_message
-                                                ),
-                                            Err(e) =>
-                                                eprintln!("Error parsing update message: {}", e),
-                                        }
-                                    }
-                                    3 => {
-                                        println!("Received notification message");
-                                        let notification_message =
-                                            structs::notificationMessage::from_bytes(&buf[19..n]);
-                                        match notification_message {
-                                            Ok(notification_message) =>
-                                                println!(
-                                                    "Received notification message: {:?}",
-                                                    notification_message
-                                                ),
-                                            Err(e) =>
-                                                eprintln!("Error parsing notification message: {}", e),
-                                        }
-                                    }
-                                    4 => {
-                                        println!("Received keepalive message");
-                                        let keepalive_message =
-                                            structs::keepaliveMessage::from_bytes(&buf[0..n]);
-                                        match keepalive_message {
-                                            Ok(keepalive_message) =>
-                                                println!(
-                                                    "Received keepalive message: {:?}",
-                                                    keepalive_message
-                                                ),
-                                            Err(e) =>
-                                                eprintln!("Error parsing keepalive message: {}", e),
-                                        }
-                                    }
-                                    _ => println!("Unknown message type: {}", header.message_type),
-                                }
+                                println!("Received header: {:?}", header);
+                                header
                             }
                             Err(e) => {
                                 eprintln!("Error parsing header: {}", e);
+                                return;
                             }
+                        };
+
+                        match header.message_type {
+                            1 => {
+                                #[cfg(debug_assertions)]
+                                println!("Received open message");
+                                match structs::openMessage::from_bytes(&buf[19..n]) {
+                                    Ok(open_message) => {
+                                        #[cfg(debug_assertions)]
+                                        println!("Received open message: {:?}", open_message);
+                                        // Send keepalive after successful open message parse
+                                        let keepalive = structs::keepaliveMessage::new(header);
+                                        let keepalive_bytes = keepalive.to_bytes();
+                                        if let Err(e) = stream.write_all(&keepalive_bytes) {
+                                            eprintln!("Failed to send keepalive: {}", e);
+                                        } else {
+                                            #[cfg(debug_assertions)]
+                                            println!("Sent keepalive message");
+                                        }
+                                    }
+                                    Err(e) => eprintln!("Error parsing open message: {}", e),
+                                }
+                            }
+                            2 => {
+                                #[cfg(debug_assertions)]
+                                println!("Received update message");
+                                match structs::updateMessage::from_bytes(&buf[19..n]) {
+                                    Ok(update_message) => {
+                                        #[cfg(debug_assertions)]
+                                        println!("Received update message: {:?}", update_message);
+                                    }
+                                    Err(e) => eprintln!("Error parsing update message: {}", e),
+                                }
+                            }
+                            3 => {
+                                #[cfg(debug_assertions)]
+                                println!("Received notification message");
+                                match structs::notificationMessage::from_bytes(&buf[19..n]) {
+                                    Ok(notification_message) => {
+                                        #[cfg(debug_assertions)]
+                                        println!(
+                                            "Received notification message: {:?}",
+                                            notification_message
+                                        );
+                                    }
+                                    Err(e) =>
+                                        eprintln!("Error parsing notification message: {}", e),
+                                }
+                            }
+                            4 => {
+                                #[cfg(debug_assertions)]
+                                println!("Received keepalive message");
+                                match structs::keepaliveMessage::from_bytes(&buf[0..n]) {
+                                    Ok(keepalive_message) => {
+                                        #[cfg(debug_assertions)]
+                                        println!(
+                                            "Received keepalive message: {:?}",
+                                            keepalive_message
+                                        );
+                                    }
+                                    Err(e) => eprintln!("Error parsing keepalive message: {}", e),
+                                }
+                            }
+                            _ => #[cfg(debug_assertions)]
+                                println!("Unknown message type: {}", header.message_type),
                         }
                     }
                     Err(e) => {
